14e3081936dcafb94d31bb70bf134e49e281a3df
Edwin Aguilar <edwintec2014@gmail.com>
2023-03-14 23:17:01 -0400

    commit

---
+++ demo/src/views/NuevaView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="about">
+        <h1>Nueva pagina</h1>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'NuevaView',
+        props:[],
+        emits:[],
+        data(){
+            return{
+            }
+        },
+        methods: { 
+        },
+        computed:{
+        },
+        mounted:{
+        },
+        components:{
+        }
+    }
+</script>
